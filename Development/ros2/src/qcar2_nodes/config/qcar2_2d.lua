bebb19000826f823efe78db0fcd4b62af7b93ff4
Huy <huyq1471@gmal.com>
2026-04-03 14:30:32 +0200

    [fix ] imu not have in pbsteam

--- Development/ros2/src/qcar2_nodes/config/qcar2_2d.lua
+++ Development/ros2/src/qcar2_nodes/config/qcar2_2d.lua
@@ -24,7 +24,7 @@ options = {
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  imu_sampling_ratio = 0.,
   landmarks_sampling_ratio = 1.,
 }
 
@@ -33,7 +33,8 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.min_range = 0.12
 TRAJECTORY_BUILDER_2D.max_range = 3.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 1e9
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
